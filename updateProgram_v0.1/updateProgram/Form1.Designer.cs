ab0825fc1d344f635d1c4501b557b2305a444a75
hys <hys@DESKTOP-BBN7CSD>
2020-03-18 16:06:17 +0900

    ip list setup

--- updateProgram_v0.1/updateProgram/Form1.Designer.cs
+++ updateProgram_v0.1/updateProgram/Form1.Designer.cs
@@ -32,6 +32,10 @@
             this.bt_DstFolder = new System.Windows.Forms.Button();
             this.txt_SrcFolderLocation = new System.Windows.Forms.TextBox();
             this.txt_DstFolderLocation = new System.Windows.Forms.TextBox();
+            this.lb_SrcDescription = new System.Windows.Forms.Label();
+            this.lb_DstDescription = new System.Windows.Forms.Label();
+            this.cb_SrcIPList = new System.Windows.Forms.ComboBox();
+            this.cb_DstIPList = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // bt_SrcFolder
@@ -56,27 +60,75 @@
             // 
             // txt_SrcFolderLocation
             // 
-            this.txt_SrcFolderLocation.Location = new System.Drawing.Point(238, 13);
+            this.txt_SrcFolderLocation.Location = new System.Drawing.Point(279, 12);
             this.txt_SrcFolderLocation.Name = "txt_SrcFolderLocation";
-            this.txt_SrcFolderLocation.Size = new System.Drawing.Size(266, 23);
+            this.txt_SrcFolderLocation.Size = new System.Drawing.Size(225, 23);
             this.txt_SrcFolderLocation.TabIndex = 2;
             // 
             // txt_DstFolderLocation
             // 
-            this.txt_DstFolderLocation.Location = new System.Drawing.Point(238, 43);
+            this.txt_DstFolderLocation.Location = new System.Drawing.Point(279, 42);
             this.txt_DstFolderLocation.Name = "txt_DstFolderLocation";
-            this.txt_DstFolderLocation.Size = new System.Drawing.Size(266, 23);
+            this.txt_DstFolderLocation.Size = new System.Drawing.Size(225, 23);
             this.txt_DstFolderLocation.TabIndex = 2;
+            // 
+            // lb_SrcDescription
+            // 
+            this.lb_SrcDescription.AutoSize = true;
+            this.lb_SrcDescription.Location = new System.Drawing.Point(12, 16);
+            this.lb_SrcDescription.Name = "lb_SrcDescription";
+            this.lb_SrcDescription.Size = new System.Drawing.Size(133, 15);
+            this.lb_SrcDescription.TabIndex = 3;
+            this.lb_SrcDescription.Text = "원본 폴더 IP & 폴더 위치";
+            // 
+            // lb_DstDescription
+            // 
+            this.lb_DstDescription.AutoSize = true;
+            this.lb_DstDescription.Location = new System.Drawing.Point(12, 46);
+            this.lb_DstDescription.Name = "lb_DstDescription";
+            this.lb_DstDescription.Size = new System.Drawing.Size(133, 15);
+            this.lb_DstDescription.TabIndex = 3;
+            this.lb_DstDescription.Text = "대상 폴더 IP & 폴더 위치";
+            // 
+            // cb_SrcIPList
+            // 
+            this.cb_SrcIPList.FormattingEnabled = true;
+            this.cb_SrcIPList.Items.AddRange(new object[] {
+            "172.16.14.10",
+            "172.16.14.24",
+            "172.16.14.18"});
+            this.cb_SrcIPList.Location = new System.Drawing.Point(152, 12);
+            this.cb_SrcIPList.Name = "cb_SrcIPList";
+            this.cb_SrcIPList.Size = new System.Drawing.Size(121, 23);
+            this.cb_SrcIPList.TabIndex = 4;
+            this.cb_SrcIPList.Text = "172.16.14.10";
+            // 
+            // cb_DstIPList
+            // 
+            this.cb_DstIPList.FormattingEnabled = true;
+            this.cb_DstIPList.Items.AddRange(new object[] {
+            "172.16.14.10",
+            "172.16.14.24",
+            "172.16.14.18"});
+            this.cb_DstIPList.Location = new System.Drawing.Point(152, 42);
+            this.cb_DstIPList.Name = "cb_DstIPList";
+            this.cb_DstIPList.Size = new System.Drawing.Size(121, 23);
+            this.cb_DstIPList.TabIndex = 4;
+            this.cb_DstIPList.Text = "172.16.14.24";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(619, 450);
-            this.Controls.Add(this.txt_DstFolderLocation);
-            this.Controls.Add(this.txt_SrcFolderLocation);
-            this.Controls.Add(this.bt_DstFolder);
+            this.Controls.Add(this.cb_DstIPList);
+            this.Controls.Add(this.cb_SrcIPList);
+            this.Controls.Add(this.lb_DstDescription);
             this.Controls.Add(this.bt_SrcFolder);
+            this.Controls.Add(this.txt_SrcFolderLocation);
+            this.Controls.Add(this.lb_SrcDescription);
+            this.Controls.Add(this.txt_DstFolderLocation);
+            this.Controls.Add(this.bt_DstFolder);
             this.Name = "Form1";
             this.Text = "Update Program v0.1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -91,6 +143,10 @@
         private System.Windows.Forms.Button bt_DstFolder;
         private System.Windows.Forms.TextBox txt_SrcFolderLocation;
         private System.Windows.Forms.TextBox txt_DstFolderLocation;
+        private System.Windows.Forms.Label lb_SrcDescription;
+        private System.Windows.Forms.Label lb_DstDescription;
+        private System.Windows.Forms.ComboBox cb_SrcIPList;
+        private System.Windows.Forms.ComboBox cb_DstIPList;
     }
 }
 
